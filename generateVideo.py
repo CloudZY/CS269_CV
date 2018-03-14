0c1f4e9255d81495d45f9ced550be920720a4322
zhanglef <zhanglef7@gmail.com>
2018-03-14 00:09:35 -0700

    UPDATE GenerateVideo

--- generateVideo.py
+++ generateVideo.py
@@ -5,15 +5,18 @@ import os
 def imgs2video(imgs_dir, save_name):
     fps = 24
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    video_writer = cv2.VideoWriter(save_name, fourcc, fps, (1920, 1080))
+    video_writer = cv2.VideoWriter(save_name, fourcc, fps, (1024, 436))
     # no glob, need number-index increasing
     imgs = glob.glob(os.path.join(imgs_dir, '*.png'))
 
-    for i in range(len(imgs)):
-        imgname = os.path.join(imgs_dir, 'core-{:02d}.png'.format(i))
-        frame = cv2.imread(imgname)
+    images = []
+    for image in os.listdir("./images_after_flow/"):
+        images.append("./images_after_flow/" + str(image))
+    #print(images)
+    for i in images:
+        frame = cv2.imread(i)
         video_writer.write(frame)
 
     video_writer.release()
 
-imgs2video("./images_after_flow","video")
+imgs2video("./images_after_flow","video.avi")
